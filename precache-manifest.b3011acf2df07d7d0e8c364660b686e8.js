59e3692ea4ef529baca78e474bf686ba6b7d6fc8
James Kim <jamesjkim8806@gmail.com>
2019-12-18 18:52:31 -0800

    Updates

---
+++ precache-manifest.b3011acf2df07d7d0e8c364660b686e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60b3010db16c962e9ec305ffa91d94d4",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "42fd59799c6d263a9f0f",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "7d795134bdd3d0c53a41",
+    "url": "/robofriends/static/css/main.8742ab47.chunk.css"
+  },
+  {
+    "revision": "42fd59799c6d263a9f0f",
+    "url": "/robofriends/static/js/2.8af48160.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends/static/js/2.8af48160.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7d795134bdd3d0c53a41",
+    "url": "/robofriends/static/js/main.91820c27.chunk.js"
+  },
+  {
+    "revision": "c9cbbbb8aecacad41a0f",
+    "url": "/robofriends/static/js/runtime-main.2f80a05c.js"
+  }
+]);
